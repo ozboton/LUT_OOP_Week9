9b026c3a1c61636bbb0d2130c1a41ecd0146da61
ozboton <55617798+ozboton@users.noreply.github.com>
2025-03-17 04:50:09 +0200

    Update AsphaltCar.java

--- week9/src/main/java/main/AsphaltCar.java
+++ week9/src/main/java/main/AsphaltCar.java
@@ -1,10 +1,12 @@
 package main;
 
+//creates the car for asphalt
 public class AsphaltCar extends RallyCar {
     public AsphaltCar(String make, String model, int horsepower) {
         super(make, model, horsepower);
     }
 
+    //calculate the performance, rounded to one digit
     @Override
     public double calculatePerformance() {
         return Math.round((horsepower * 1.15 + 25) * 10.0) / 10.0;
