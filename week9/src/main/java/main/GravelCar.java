4238002dea699b1df275e398e775569689c3821a
ozboton <55617798+ozboton@users.noreply.github.com>
2025-03-17 04:56:12 +0200

    Update GravelCar.java

--- week9/src/main/java/main/GravelCar.java
+++ week9/src/main/java/main/GravelCar.java
@@ -1,10 +1,12 @@
 package main;
 
+//creates car for gravel
 public class GravelCar extends RallyCar {
     public GravelCar(String make, String model, int horsepower) {
         super(make, model, horsepower);
     }
 
+    //calculate the performance, rounded to one digit
     @Override
     public double calculatePerformance() {
         return Math.round((horsepower * 1.05 + 40) * 10.0) / 10.0;
