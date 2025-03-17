1115aa7748c3fcf71ae9b95191d22d3af1cf60ea
ozboton <55617798+ozboton@users.noreply.github.com>
2025-03-17 04:54:19 +0200

    Update ChampionshipStatistics.java

--- week9/src/main/java/main/ChampionshipStatistics.java
+++ week9/src/main/java/main/ChampionshipStatistics.java
@@ -2,18 +2,18 @@ package main;
 
 import java.util.List;
 
-
+//calcs average points for drivers
 public class ChampionshipStatistics {
     public static double averagePointsPerDriver(List<Driver> drivers) {
         return drivers.stream().mapToInt(Driver::getTotalPoints).average().orElse(0.0);
     }
-
+    //gets the country of the driver with the most points
     public static String mostSuccessfulCountry(List<Driver> drivers) {
         return drivers.stream()
                 .max((a, b) -> Integer.compare(a.getTotalPoints(), b.getTotalPoints()))
                 .map(Driver::getCountry).orElse("None");
     }
-
+    //calcs all points for drivers
     public static int totalChampionshipPoints(List<Driver> drivers) {
         return drivers.stream().mapToInt(Driver::getTotalPoints).sum();
     }
